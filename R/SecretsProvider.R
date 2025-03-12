0ada3a8add54ead969fdf933ba3f809aa8977e2e
KoenGreuell <koengreuell@gmail.com>
2025-03-12 17:56:10 +0100

    Update documentation, apply linter suggestions

--- R/SecretsProvider.R
+++ R/SecretsProvider.R
@@ -1,27 +1,27 @@
 SecretsProvider <- function(secret_file_path = ".env", input_func = getPass::getPass) {
   self <- list()
-  
+
   self$secret_file_path <- secret_file_path
   self$get_input <- input_func
-  
+
   self$ensure_file_exists <- function() {
     if (!file.exists(self$secret_file_path)) {
       file.create(self$secret_file_path)
     }
   }
-  
+
   self$ensure_file_exists()
-  
+
   self$set_secret <- function(secret_name) {
     current_secret_value <- self$get_secret(secret_name)
     if (!is.null(current_secret_value)) {
-      cat(paste0("\nThere is already a secret named ", secret_name, " stored as ", 
+      cat(paste0("\nThere is already a secret named ", secret_name, " stored as ",
                  secret_name, ". Entering a new value will overwrite the existing secret.\n"))
     }
     new_secret_value <- self$request_secret_value()
     self$store_secret(secret_name, new_secret_value)
   }
-  
+
   self$read_file_to_dict <- function() {
     # Read the lines from the file
     lines <- readLines(self$secret_file_path)
@@ -38,29 +38,28 @@ SecretsProvider <- function(secret_file_path = ".env", input_func = getPass::get
         result_dict[[key]] <- value
       }
     }
-    return(result_dict)
+    result_dict
   }
-  
+
   self$get_secret <- function(secret_name) {
     if (file.info(self$secret_file_path)$size == 0) {
       secret_dict <- self$read_file_to_dict()
       secret_value <- secret_dict$secret_name
-    }
-    else {
-     secret_value <- NULL 
+    } else {
+      secret_value <- NULL
     }
     if (is.null(secret_value)) {
       cat(paste0("\nA secret with name ", secret_name, " is not yet stored in ", self$secret_file_path, ".\n"))
       secret_value <- self$request_secret_value()
       self$store_secret(secret_name, secret_value)
     }
-    return(secret_value)
+    secret_value
   }
-  
+
   self$request_secret_value <- function() {
-    return(self$get_input("Enter your Secret: "))
+    self$get_input("Enter your Secret: ")
   }
-  
+
   self$store_secret <- function(secret_name, secret_value) {
     if (nchar(trimws(secret_value)) > 0) {
       self$insert_key_value_into_file(secret_name, secret_value)
@@ -69,7 +68,7 @@ SecretsProvider <- function(secret_file_path = ".env", input_func = getPass::get
       cat("\nNo value entered for the secret. Not storing.\n")
     }
   }
-  
+
   self$write_dict_to_file <- function(dict) {
     file_conn <- file(self$secret_file_path, "w")
     for (key in names(dict)) {
@@ -78,17 +77,17 @@ SecretsProvider <- function(secret_file_path = ".env", input_func = getPass::get
     }
     close(file_conn)
   }
-  
+
   self$insert_key_value_into_file <- function(secret_name, value) {
     existing_dict <- self$read_file_to_dict()
     existing_dict[[secret_name]] <- value
     self$write_dict_to_file(existing_dict)
   }
-  
+
   self$remove_secret <- function(secret_name) {
     self$insert_key_value_into_file(secret_name, NULL)
     cat(paste0("\nSecret ", secret_name, " has been removed.\n"))
   }
-  
+
   return(self)
 }
--- tests/testthat/testthat.R
+++ tests/testthat/testthat.R
@@ -16,33 +16,33 @@ test_that("test_set_secret_new_secret", {
   secret1_name <- "secret1"
   secret1_value <- "secret1_value"
   mock_input <- mock_input_factory(secret1_value)
-  
+
   expect_equal(secret1_value, SecretsProvider(input_func = mock_input, secret_file_path = test_env_path)$get_secret(secret1_name))
-  
+
   unlink(test_env_path)
 })
 
 test_that("test_set_secret_no_input", {
   secret1_name <- "secret1"
   mock_input <- mock_input_factory("")
-  secretsProvider <- SecretsProvider(input_func = mock_input, secret_file_path = test_env_path)
-  
-  secretsProvider$set_secret(secret1_name)
-  
-  expect_equal(0, length(secretsProvider$read_file_to_dict()))
-  
+  secrets_provider <- SecretsProvider(input_func = mock_input, secret_file_path = test_env_path)
+
+  secrets_provider$set_secret(secret1_name)
+
+  expect_equal(0, length(secrets_provider$read_file_to_dict()))
+
   unlink(test_env_path)
 })
 
 test_that("test_set_secret_space_input", {
   secret1_name <- "secret1"
   mock_input <- mock_input_factory(" ")
-  secretsProvider <- SecretsProvider(input_func = mock_input, secret_file_path = test_env_path)
-  
-  secretsProvider$set_secret(secret1_name)
-  
-  expect_equal(0, length(secretsProvider$read_file_to_dict()))
-  
+  secrets_provider <- SecretsProvider(input_func = mock_input, secret_file_path = test_env_path)
+
+  secrets_provider$set_secret(secret1_name)
+
+  expect_equal(0, length(secrets_provider$read_file_to_dict()))
+
   unlink(test_env_path)
 })
 
@@ -51,13 +51,13 @@ test_that("test_set_secret_overwrite", {
   secret1_original_value <- "secret1_original_value"
   secret1_new_value <- "secret1_new_value"
   mock_input <- mock_input_factory(secret1_new_value)
-  secretsProvider <- SecretsProvider(input_func = mock_input, secret_file_path = test_env_path)
-  
-  secretsProvider$set_secret(secret1_original_value)
-  secretsProvider$set_secret(secret1_name)
-  
-  expect_equal(secret1_new_value, secretsProvider$read_file_to_dict()[[secret1_name]])
-  
+  secrets_provider <- SecretsProvider(input_func = mock_input, secret_file_path = test_env_path)
+
+  secrets_provider$set_secret(secret1_original_value)
+  secrets_provider$set_secret(secret1_name)
+
+  expect_equal(secret1_new_value, secrets_provider$read_file_to_dict()[[secret1_name]])
+
   unlink(test_env_path)
 })
 
@@ -65,12 +65,12 @@ test_that("test_get_secret_nonexistent", {
   secret1_name <- "secret1"
   secret1_value <- "secret1_value"
   mock_input <- mock_input_factory(secret1_value)
-  secretsProvider <- SecretsProvider(input_func = mock_input, secret_file_path = test_env_path)
-  
-  secret_value <- secretsProvider$get_secret(secret1_name)
-  
-  expect_equal(secret1_value, secretsProvider$read_file_to_dict()[[secret1_name]])
-  
+  secrets_provider <- SecretsProvider(input_func = mock_input, secret_file_path = test_env_path)
+
+  secret_value <- secrets_provider$get_secret(secret1_name)
+
+  expect_equal(secret1_value, secrets_provider$read_file_to_dict()[[secret1_name]])
+
   unlink(test_env_path)
 })
 
@@ -78,12 +78,12 @@ test_that("test_get_secret_existent", {
   secret1_name <- "secret1"
   secret1_value <- "secret1_value"
   mock_input <- mock_input_factory(secret1_value)
-  secretsProvider <- SecretsProvider(input_func = mock_input, secret_file_path = test_env_path)
-  
-  secretsProvider$set_secret(secret1_name)
-  
-  expect_equal(secret1_value, secretsProvider$read_file_to_dict()[[secret1_name]])
-  
+  secrets_provider <- SecretsProvider(input_func = mock_input, secret_file_path = test_env_path)
+
+  secrets_provider$set_secret(secret1_name)
+
+  expect_equal(secret1_value, secrets_provider$read_file_to_dict()[[secret1_name]])
+
   unlink(test_env_path)
 })
 
@@ -92,14 +92,14 @@ test_that("test_remove_secret", {
   secret2_name <- "secret2"
   secret_value <- "secret_value"
   mock_input <- mock_input_factory(secret_value)
-  secretsProvider <- SecretsProvider(input_func = mock_input, secret_file_path = test_env_path)
-  
-  secretsProvider$set_secret(secret1_name)
-  secretsProvider$set_secret(secret2_name)
-  secretsProvider$remove_secret(secret1_name)
-  
-  expect_equal(1, length(secretsProvider$read_file_to_dict()))
-  
+  secrets_provider <- SecretsProvider(input_func = mock_input, secret_file_path = test_env_path)
+
+  secrets_provider$set_secret(secret1_name)
+  secrets_provider$set_secret(secret2_name)
+  secrets_provider$remove_secret(secret1_name)
+
+  expect_equal(1, length(secrets_provider$read_file_to_dict()))
+
   unlink(test_env_path)
 })
 
@@ -107,12 +107,12 @@ test_that("test_remove_last_secret", {
   secret1_name <- "secret1"
   secret_value <- "secret_value"
   mock_input <- mock_input_factory(secret_value)
-  secretsProvider <- SecretsProvider(input_func = mock_input, secret_file_path = test_env_path)
-  
-  secretsProvider$set_secret(secret1_name)
-  secretsProvider$remove_secret(secret1_name)
+  secrets_provider <- SecretsProvider(input_func = mock_input, secret_file_path = test_env_path)
 
-  expect_equal(0, length(secretsProvider$read_file_to_dict()))
-  
+  secrets_provider$set_secret(secret1_name)
+  secrets_provider$remove_secret(secret1_name)
+
+  expect_equal(0, length(secrets_provider$read_file_to_dict()))
+
   unlink(test_env_path)
 })
