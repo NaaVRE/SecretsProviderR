251f11603ebc33ed8cc79f00c1f1c9b905de6297
KoenGreuell <koengreuell@gmail.com>
2025-03-13 16:35:37 +0100

    Refactor method order

--- R/SecretsProvider.R
+++ R/SecretsProvider.R
@@ -4,6 +4,38 @@ SecretsProvider <- function(input_func = getPass::getPass, secret_file_path = ".
   self$secret_file_path <- secret_file_path
   self$get_input <- input_func
 
+  self$get_secret <- function(secret_name) {
+    secret_value <- self$retrieve_secret(secret_name)
+    if (is.null(secret_value)) {
+      prompt <- paste0("A secret with name ",
+                       secret_name,
+                       " is not yet stored in the file ",
+                       self$secret_file_path,
+                       ". Enter your Secret")
+      secret_value <- self$request_secret_value(prompt)
+      self$store_secret(secret_name, secret_value)
+    }
+    secret_value
+  }
+
+  self$set_secret <- function(secret_name) {
+    current_secret_value <- self$retrieve_secret(secret_name)
+    prompt <- "Enter your Secret"
+    if (!is.null(current_secret_value)) {
+      prompt <- paste0("There is already a secret named ",
+                       secret_name,
+                       ". Entering a new value will overwrite the existing secret. ",
+                       prompt)
+    }
+    new_secret_value <- self$request_secret_value(prompt)
+    self$store_secret(secret_name, new_secret_value)
+  }
+
+  self$remove_secret <- function(secret_name) {
+    self$insert_key_value_into_file(secret_name, NULL)
+    cat(paste0("\nSecret ", secret_name, " has been removed.\n"))
+  }
+
   self$read_file_to_dict <- function() {
     lines <- readLines(self$secret_file_path)
     result_dict <- list()
@@ -27,33 +59,6 @@ SecretsProvider <- function(input_func = getPass::getPass, secret_file_path = ".
     } else {
       file.create(self$secret_file_path)
       secret_value <- NULL
-    }
-    secret_value
-  }
-
-  self$set_secret <- function(secret_name) {
-    current_secret_value <- self$retrieve_secret(secret_name)
-    prompt <- "Enter your Secret"
-    if (!is.null(current_secret_value)) {
-      prompt <- paste0("There is already a secret named ",
-                       secret_name,
-                       ". Entering a new value will overwrite the existing secret. ",
-                       prompt)
-    }
-    new_secret_value <- self$request_secret_value(prompt)
-    self$store_secret(secret_name, new_secret_value)
-  }
-
-  self$get_secret <- function(secret_name) {
-    secret_value <- self$retrieve_secret(secret_name)
-    if (is.null(secret_value)) {
-      prompt <- paste0("A secret with name ",
-                       secret_name,
-                       " is not yet stored in the file ",
-                       self$secret_file_path,
-                       ". Enter your Secret")
-      secret_value <- self$request_secret_value(prompt)
-      self$store_secret(secret_name, secret_value)
     }
     secret_value
   }
@@ -84,11 +89,6 @@ SecretsProvider <- function(input_func = getPass::getPass, secret_file_path = ".
     existing_dict <- self$read_file_to_dict()
     existing_dict[[secret_name]] <- value
     self$write_dict_to_file(existing_dict)
-  }
-
-  self$remove_secret <- function(secret_name) {
-    self$insert_key_value_into_file(secret_name, NULL)
-    cat(paste0("\nSecret ", secret_name, " has been removed.\n"))
   }
 
   return(self)
